022b4dcfabe4218a8611a88ce4b3b594555eea42
Abhishek <gowdaabhishek40641@gmail.com>
2026-09-25 14:16:27 +0530

    Add multiple face toggle functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ```javascript
 var face = document.getElementById("face");
 var faceName = document.getElementById("faceName");
+var message = document.getElementById("message");
 
 var faceNumber = 1;
 
@@ -11,35 +12,36 @@ function toggleFace() {
 
         face.innerHTML = "😢";
         faceName.innerHTML = "Sad Face";
+        message.innerHTML = "I am feeling sad!";
 
         faceNumber = 2;
-
     }
 
     else if (faceNumber == 2) {
 
         face.innerHTML = "😡";
         faceName.innerHTML = "Angry Face";
+        message.innerHTML = "I am feeling angry!";
 
         faceNumber = 3;
-
     }
 
     else if (faceNumber == 3) {
 
         face.innerHTML = "😲";
         faceName.innerHTML = "Surprised Face";
+        message.innerHTML = "I am surprised!";
 
         faceNumber = 4;
-
     }
 
     else {
 
         face.innerHTML = "😊";
         faceName.innerHTML = "Happy Face";
+        message.innerHTML = "I am feeling happy!";
 
         faceNumber = 1;
-
     }
 }
+```
